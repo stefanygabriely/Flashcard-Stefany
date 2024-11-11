57305798a27302ad86d1581cd5ccae288f2f3e0c
stefanygabriely <105082677+stefanygabriely@users.noreply.github.com>
2024-11-11 11:29:07 -0300

    ok

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+function criarCartao(categoia,pergunta,resposta){
+//console.log(categoria,pergunta,resposta)
+let container= document.getElementById('container');
+let cartao=document.createElement('article')
+cartao.classname='cartao';
+cartao.innerHTML=
+<div class="cartao__contudo">
+    <h3>programacao</h3>
+    
+</div>
+}
+    
