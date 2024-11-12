df29a37b43f6cafdad71c0b418f1b26293493ca6
stefanygabriely <105082677+stefanygabriely@users.noreply.github.com>
2024-11-12 09:34:10 -0300

    kk

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,23 @@
 function criarCartao(categoia,pergunta,resposta){
 //console.log(categoria,pergunta,resposta)
 let container= document.getElementById('container');
+//VARIAVEL CONTAINER BUSCADA PELO ID 'container'
 let cartao=document.createElement('article')
+//VARIAVEL CARTAO QUE CRIA <ARTICLE> NO ATML
 cartao.classname='cartao';
+//CRIA CLASS"cartao" para o elemento criado
 cartao.innerHTML=
 <div class="cartao__contudo">
-    <h3>programacao</h3>
-    
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__resposta"></div>
+
 </div>
 }
-    
+    let respostaEstaVisivel= false
+    function viraCartao(){
+        respostaEstaVisivel=!respostaEstaVisivel
+        cartao.classList.toggle('active',respostaEstaVisivel)
+    }
+    cartao.addEventListener('click',viraCartao)
+    container.appendChild(cartao)
+
--- perguntas.js
+++ perguntas.js
@@ -2,7 +2,7 @@ criaCartao()
 
 
 criaCartao(
-    categoria,
+    programcao,
     pergunta,
     resposta,
 )
